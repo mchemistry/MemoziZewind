dfbe3a6a88d6a2354c3f955f93efe2fa74325e44
Tân Dv <mchemistry95@gmail.com>
2020-09-17 10:54:26 +0700

    Update TextEmoji Component

--- components/TextEmoji/index.vue
+++ components/TextEmoji/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <span class="text-emoji" :style="style">
-    em laf ai
-    <img
-      class="emoji-icon"
-      src="../../assets/images/emoji/big-smile.gif"
-      alt="Emoji Icon"
-      :style="{ height: size + 'px' }"
-    />
+  <span class="text-emoji" :style="style" v-html="convertToTempalte(text)">
   </span>
 </template>
 
 <script>
+import _ from 'lodash'
+const EMOJI_SHOTHAND = ['a', 'b', ':dog', ':D']
+const GIF_SOURCE = [
+  'https://data2.m4v.me/images/smileys/simply/(.gif',
+  'https://data2.m4v.me/images/smileys/simply/(.gif',
+  'https://data2.m4v.me/images/smileys/simply/(.gif',
+  'https://data2.m4v.me/images/smileys/simply/(.gif',
+]
 export default {
   name: 'TextEmoji',
   props: {
@@ -28,6 +29,26 @@ export default {
       const style = {}
       style['font-size'] = this.size + 'px'
       return style
+    },
+  },
+  methods: {
+    convertToTempalte(text) {
+      let result
+      for (let i = 0; i < EMOJI_SHOTHAND.length; i++) {
+        for (let j = 0; j < EMOJI_SHOTHAND.length; j++) {
+          result = _.replace(
+            text,
+            new RegExp(EMOJI_SHOTHAND[i], 'g'),
+            `<img
+              class="emoji-icon"
+              src="${GIF_SOURCE[j]}"
+              alt="Emoji Icon"
+              style="height: ${this.size}px !important; position: relative; top: 3px;"
+            />`
+          )
+        }
+      }
+      return result
     },
   },
 }
