207350e20ccc4218979d20e707a3e1a008414256
Tân Dv <mchemistry95@gmail.com>
2020-09-20 16:33:47 +0700

    Add files via upload

--- utils/conver-images.js
+++ utils/conver-images.js
@@ -1,4 +1,4 @@
-const text = `<a href="https://imgbb.com/"><img src="https://i.ibb.co/FWHMzdj/1.png" alt="1" border="0"></a>
+const thumnailLinks = `<a href="https://imgbb.com/"><img src="https://i.ibb.co/FWHMzdj/1.png" alt="1" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/KypnXPj/2.png" alt="2" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/fGWp97P/3.png" alt="3" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/mJhns09/4.png" alt="4" border="0"></a>
@@ -20,10 +20,55 @@ const text = `<a href="https://imgbb.com/"><img src="https://i.ibb.co/FWHMzdj/1.
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/LR18kmm/20.png" alt="20" border="0"></a>
 <a href="https://ibb.co/PgLqWLb"><img src="https://i.ibb.co/89C1xCR/21.png" alt="21" border="0"></a>
 <a href="https://imgbb.com/"><img src="https://i.ibb.co/X5Lsx82/22.png" alt="22" border="0"></a>`
-const arrImagesEmbed = text.split(`\n`)
+const imageLinks = `<a href="https://ibb.co/55Gr97y"><img src="https://i.ibb.co/Jzmvdh4/1.png" alt="1" border="0"></a>
+<a href="https://ibb.co/yXLDtCp"><img src="https://i.ibb.co/L84Mmyr/2.png" alt="2" border="0"></a>
+<a href="https://ibb.co/0qsdQQx"><img src="https://i.ibb.co/H7hkXXj/3.png" alt="3" border="0"></a>
+<a href="https://ibb.co/KqZwgVJ"><img src="https://i.ibb.co/zRcJgPz/4.png" alt="4" border="0"></a>
+<a href="https://ibb.co/jf7Pn7N"><img src="https://i.ibb.co/mTQVfQj/5.png" alt="5" border="0"></a>
+<a href="https://ibb.co/Yfr8Hqz"><img src="https://i.ibb.co/zPkbq0y/6.png" alt="6" border="0"></a>
+<a href="https://ibb.co/svwW0cT"><img src="https://i.ibb.co/DDCrxmB/7.png" alt="7" border="0"></a>
+<a href="https://ibb.co/Y72SXK7"><img src="https://i.ibb.co/wNyq7bN/8.png" alt="8" border="0"></a>
+<a href="https://ibb.co/2FdZRGR"><img src="https://i.ibb.co/nwjP4x4/9.png" alt="9" border="0"></a>
+<a href="https://ibb.co/SnGgDP9"><img src="https://i.ibb.co/3NQb57L/10.png" alt="10" border="0"></a>
+<a href="https://ibb.co/7QDf1nz"><img src="https://i.ibb.co/Scgj3tx/11.png" alt="11" border="0"></a>
+<a href="https://ibb.co/v41cF6R"><img src="https://i.ibb.co/9HnpDkS/12.png" alt="12" border="0"></a>
+<a href="https://ibb.co/YDB7sfh"><img src="https://i.ibb.co/B42VMTB/13.png" alt="13" border="0"></a>
+<a href="https://ibb.co/KLbG54t"><img src="https://i.ibb.co/5nG91pQ/14.png" alt="14" border="0"></a>
+<a href="https://ibb.co/tJRBVPn"><img src="https://i.ibb.co/z4dsqHC/15.png" alt="15" border="0"></a>
+<a href="https://ibb.co/TK2vJFC"><img src="https://i.ibb.co/s5RmcMB/16.png" alt="16" border="0"></a>
+<a href="https://ibb.co/k6MBdX6"><img src="https://i.ibb.co/Z1NXZx1/17.png" alt="17" border="0"></a>
+<a href="https://ibb.co/pKCKy3Z"><img src="https://i.ibb.co/t2y2Xbc/18.png" alt="18" border="0"></a>
+<a href="https://ibb.co/C11QV0P"><img src="https://i.ibb.co/Fxxz87H/19.png" alt="19" border="0"></a>
+<a href="https://ibb.co/RbQ4Kks"><img src="https://i.ibb.co/T2PqGx7/20.png" alt="20" border="0"></a>
+<a href="https://ibb.co/QbsctLk"><img src="https://i.ibb.co/VjXN5Rx/21.png" alt="21" border="0"></a>
+<a href="https://ibb.co/c1ntb3v"><img src="https://i.ibb.co/W6JFD53/22.png" alt="22" border="0"></a>`
 
-console.log(arrImagesEmbed[0].split(`"`))
-for (const i in arrImagesEmbed) {
-    arrImagesEmbed[i] = arrImagesEmbed[i].split(`"`)[3]
+const arrThumnailsEmbed = thumnailLinks.split(`\n`)
+const arrImageEmbed = imageLinks.split(`\n`)
+
+let convertLinkFromEmbed = (arr) => {
+    for (const i in arr) {
+        arr[i] = arr[i].split(`"`)[3]
+    }
+    return arr
 }
-console.log(arrImagesEmbed)
+
+const thumnailLinksConverted = convertLinkFromEmbed(arrThumnailsEmbed)
+const imageLinksConverted = convertLinkFromEmbed(arrImageEmbed)
+let syncImageLinksToObject = () => {
+    let arrResult = []
+    const result = {}
+    for (const i in thumnailLinksConverted) {
+        arrResult.push({
+            src: imageLinksConverted[i],
+            thumbnail: thumnailLinksConverted[i],
+            w: 360,
+            h: 360,
+            alt: 'My Love Image',
+            title: 'Make with heart !!!'
+        })
+    }
+    return arrResult
+}
+
+console.log(syncImageLinksToObject())
