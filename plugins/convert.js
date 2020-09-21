28e72608a732ba8c3acce734930df78da85064c0
Tân Dv <mchemistry95@gmail.com>
2020-09-21 21:46:39 +0700

    Update Gallery Page

--- plugins/convert.js
+++ plugins/convert.js
@@ -1,95 +1,69 @@
 /* jshint esversion: 6 */
-const str = `[00:06.19]Nice to meet you, where you been?
-[00:08.46]I could show you incredible things
-[00:11.01]Magic, madness, heaven, sin
-[00:13.45]Saw you there, and I thought
-[00:15.46]"Oh my God, look at that face
-[00:17.92]You look like my next mistake
-[00:20.46]Love's a game, wanna play?"
-[00:23.79]Ayy
-[00:25.37]...
-[00:25.98]New money, suit and tie
-[00:28.47]I can read you like a magazine
-[00:31.01]Ain't it funny? Rumors fly
-[00:33.48]And I know you heard about me
-[00:35.78]So hey, let's be friends
-[00:37.78]I'm dyin' to see how this one ends
-[00:40.31]Grab your passport and my hand
-[00:42.88]I can make the bad guys good for a weekend
-[00:44.51]...
-[00:45.55]So it's gonna be forever
-[00:48.09]Or it's gonna go down in flames
-[00:50.58]You can tell me when it's over, mm
-[00:53.13]If the high was worth the pain
-[00:55.58]Got a long list of ex-lovers
-[00:58.01]They'll tell you I'm insane
-[01:00.53]'Cause you know I love the players
-[01:02.90]And you love the game
-[01:05.53]'Cause we're young and we're reckless
-[01:08.00]We'll take this way too far
-[01:10.50]It'll leave you breathless, hmm
-[01:13.19]Or with a nasty scar
-[01:15.58]Got a long list of ex-lovers
-[01:18.05]They'll tell you I'm insane
-[01:20.70]But I've got a blank space, baby
-[01:24.26]And I'll write your name
-[01:25.34]...
-[01:25.79]Cherry lips, crystal skies
-[01:33.53]I could show you incredible things
-[01:35.93]Stolen kisses, pretty lies
-[01:38.47]You're the King, baby, I'm your Queen
-[01:40.92]Find out what you want
-[01:42.71]Be that girl for a month
-[01:45.25]Wait, the worst is yet to come
-[01:49.18]Oh, no
-[01:50.44]...
-[01:50.85]Screaming, crying, perfect storms
-[01:53.45]I can make all the tables turn
-[01:55.93]Rose garden filled with thorns
-[01:58.45]Keep you second guessin' like
-[02:00.54]"Oh my God, who is she?"
-[02:02.69]I get drunk on jealousy
-[02:05.41]But you'll come back each time you leave
-[02:07.78]'Cause, darling, I'm a nightmare dressed like a daydream
-[02:10.02]...
-[02:10.58]So it's gonna be forever
-[02:13.25]Or it's gonna go down in flames
-[02:15.68]You can tell me when it's over, mm
-[02:18.25]If the high was worth the pain
-[02:20.55]Got a long list of ex-lovers
-[02:22.94]They'll tell you I'm insane
-[02:25.58]'Cause you know I love the players
-[02:28.12]And you love the game
-[02:30.61]'Cause we're young and we're reckless
-[02:33.25]We'll take this way too far
-[02:35.60]It'll leave you breathless, hmm
-[02:38.22]Or with a nasty scar
-[02:40.62]Got a long list of ex-lovers
-[02:43.01]They'll tell you I'm insane
-[02:45.58]But I've got a blank space, baby
-[02:47.81]And I'll write your name
-[02:50.87]...
-[02:51.39]Boys only want love if it's torture
-[02:55.58]Don't say I didn't, say I didn't warn ya
-[03:00.34]Boys only want love if it's torture
-[03:05.42]Don't say I didn't, say I didn't warn ya
-[03:09.93]...
-[03:10.51]So it's gonna be forever
-[03:13.13]Or it's gonna go down in flames
-[03:15.60]You can tell me when it's over, mm
-[03:18.09]If the high was worth the pain
-[03:20.49]Got a long list of ex-lovers
-[03:22.93]They'll tell you I'm insane
-[03:25.47]'Cause you know I love the players
-[03:28.10]And you love the game
-[03:30.71]'Cause we're young and we're reckless
-[03:33.00]We'll take this way too far
-[03:35.41]It'll leave you breathless, hmm
-[03:38.09]Or with a nasty scar
-[03:40.58]Got a long list of ex-lovers
-[03:43.05]They'll tell you I'm insane
-[03:45.81]But I've got a blank space, baby
-[03:49.65]And I'll write your name
+const str = `[00:19.19]Mãi sau này anh mới biết
+[00:22.24]Bông hoa đó không phải của anh
+[00:26.95]Chẳng qua là anh đã đi ngang qua
+[00:30.29]Đúng mùa hoa đẹp nhất
+[00:36.14]Con tim anh cứ ngỡ là duyên số
+[00:40.13]Thì ra đó chỉ là chuyện hư vô
+[00:44.52]Lý do chia tay là gì em có biết không
+[00:51.38]Vì em không yêu anh như anh yêu em
+[00:55.41]Vì em xem anh chỉ là nhất thời
+[01:00.69]Người mới chơi vơi như anh
+[01:04.68]Làm sao anh mơ có em
+[01:08.58]Mãi sau này xa nhau anh mới thấu
+[01:12.73]Suốt chặng đường khi yêu ai biết đâu
+[01:16.87]Người mình từng thương
+[01:18.91]Giờ như hai người xa lạ
+[01:21.22]Đã biết hết về nhau
+[01:24.43]Sau này gặp nhau anh muốn biết
+[01:27.89]Mùa hoa đẹp nhất em có nhớ gì
+[01:32.97]Ngày đó yêu nhau lúc xuân thì
+[01:36.87]Mà điều gì làm em quay lưng đi
+[01:40.98]Nếu gặp lại nhau em vẫn ôm anh
+[01:45.41]Hay em sẽ né tránh anh
+[01:49.48]Chỉ dám buông đôi câu chào
+[01:52.69]Dạo này anh sao
+[02:12.99]Mãi sau này anh mới biết
+[02:17.05]Người đó không phải người yêu anh
+[02:21.16]Chẳng qua là anh đã đi ngang qua
+[02:24.12]Đúng ngay người cô đơn nhất
+[02:29.37]Thì thầm vài câu mình nói yêu nhau
+[02:33.35]Thế nên chuyện mình cũng chẳng tới đâu
+[02:37.99]Lí do chia tay là gì em có biết không?
+[02:45.27]Vì em không yêu anh như anh yêu em
+[02:49.21]Vì em xem anh chỉ là nhất thời
+[02:54.50]Người mới chơi vơi như anh
+[02:58.28]Làm sao anh mơ có em
+[03:02.34]Mãi sau này xa nhau anh mới thấu
+[03:06.53]Suốt chặng đường khi yêu ai biết đâu
+[03:11.00]Người mình từng thương
+[03:12.87]Giờ như hai người xa lạ
+[03:15.23]Đã biết hết về nhau
+[03:18.24]Sau này gặp nhau anh muốn biết
+[03:21.66]Mùa hoa đẹp nhất em có nhớ gì
+[03:26.99]Ngày đó yêu nhau lúc xuân thì
+[03:30.85]Mà điều gì làm em quay lưng đi
+[03:34.51]Nếu gặp lại nhau em vẫn ôm anh
+[03:39.27]Hay em sẽ né tránh anh
+[03:43.29]Chỉ dám buông đôi câu chào
+[03:46.62]Dạo này anh sao
+[03:50.61]Vì em không yêu anh như anh yêu em
+[03:54.31]Vì em xem anh chỉ là nhất thời
+[03:59.80]Người mới chơi vơi như anh
+[04:03.90]Làm sao anh mơ có em
+[04:07.60]Mãi sau này xa nhau anh mới thấu
+[04:11.59]Suốt chặng đường khi yêu ai biết đâu
+[04:15.98]Người mình từng thương
+[04:18.10]Giờ như hai người xa lạ
+[04:20.13]Đã biết hết về nhau
+[04:22.93]Sau này gặp nhau anh muốn biết
+[04:26.63]Mùa hoa đẹp nhất em có nhớ gì
+[04:31.72]Ngày đó yêu nhau lúc xuân thì
+[04:35.79]Mà điều gì làm em quay lưng đi
+[04:39.81]Nếu gặp lại nhau em vẫn ôm anh
+[04:44.29]Hay em sẽ né tránh anh
+[04:48.32]Chỉ dám buông đôi câu chào
+[04:51.73]Dạo này anh sao
 `
 const arr = str.split('\n')
 const arrLyzik = []
--- utils/convert-images.js
+++ utils/convert-images.js
@@ -1,47 +1,59 @@
-const thumnailLinks = `<a href="https://imgbb.com/"><img src="https://i.ibb.co/FWHMzdj/1.png" alt="1" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/KypnXPj/2.png" alt="2" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/fGWp97P/3.png" alt="3" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/mJhns09/4.png" alt="4" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/TmpMb1P/5.png" alt="5" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/56zcfb0/6.png" alt="6" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/LYsZPK5/7.png" alt="7" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/SVVC0y7/8.png" alt="8" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/frbZn9n/9.png" alt="9" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/VtCjVd0/10.png" alt="10" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/R7m7MMV/11.png" alt="11" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/yRZPxC7/12.png" alt="12" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/stbM4J7/13.png" alt="13" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/31QNB4d/14.png" alt="14" border="0"></a>
-<a href="https://ibb.co/P5G2szh"><img src="https://i.ibb.co/8jgyn45/15.png" alt="15" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/sQMZqP1/16.png" alt="16" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/x3khsZM/17.png" alt="17" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/267csSN/18.png" alt="18" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/qJXZ1mp/19.png" alt="19" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/LR18kmm/20.png" alt="20" border="0"></a>
-<a href="https://ibb.co/PgLqWLb"><img src="https://i.ibb.co/89C1xCR/21.png" alt="21" border="0"></a>
-<a href="https://imgbb.com/"><img src="https://i.ibb.co/X5Lsx82/22.png" alt="22" border="0"></a>`
-const imageLinks = `<a href="https://ibb.co/55Gr97y"><img src="https://i.ibb.co/Jzmvdh4/1.png" alt="1" border="0"></a>
-<a href="https://ibb.co/yXLDtCp"><img src="https://i.ibb.co/L84Mmyr/2.png" alt="2" border="0"></a>
-<a href="https://ibb.co/0qsdQQx"><img src="https://i.ibb.co/H7hkXXj/3.png" alt="3" border="0"></a>
-<a href="https://ibb.co/KqZwgVJ"><img src="https://i.ibb.co/zRcJgPz/4.png" alt="4" border="0"></a>
-<a href="https://ibb.co/jf7Pn7N"><img src="https://i.ibb.co/mTQVfQj/5.png" alt="5" border="0"></a>
-<a href="https://ibb.co/Yfr8Hqz"><img src="https://i.ibb.co/zPkbq0y/6.png" alt="6" border="0"></a>
-<a href="https://ibb.co/svwW0cT"><img src="https://i.ibb.co/DDCrxmB/7.png" alt="7" border="0"></a>
-<a href="https://ibb.co/Y72SXK7"><img src="https://i.ibb.co/wNyq7bN/8.png" alt="8" border="0"></a>
-<a href="https://ibb.co/2FdZRGR"><img src="https://i.ibb.co/nwjP4x4/9.png" alt="9" border="0"></a>
-<a href="https://ibb.co/SnGgDP9"><img src="https://i.ibb.co/3NQb57L/10.png" alt="10" border="0"></a>
-<a href="https://ibb.co/7QDf1nz"><img src="https://i.ibb.co/Scgj3tx/11.png" alt="11" border="0"></a>
-<a href="https://ibb.co/v41cF6R"><img src="https://i.ibb.co/9HnpDkS/12.png" alt="12" border="0"></a>
-<a href="https://ibb.co/YDB7sfh"><img src="https://i.ibb.co/B42VMTB/13.png" alt="13" border="0"></a>
-<a href="https://ibb.co/KLbG54t"><img src="https://i.ibb.co/5nG91pQ/14.png" alt="14" border="0"></a>
-<a href="https://ibb.co/tJRBVPn"><img src="https://i.ibb.co/z4dsqHC/15.png" alt="15" border="0"></a>
-<a href="https://ibb.co/TK2vJFC"><img src="https://i.ibb.co/s5RmcMB/16.png" alt="16" border="0"></a>
-<a href="https://ibb.co/k6MBdX6"><img src="https://i.ibb.co/Z1NXZx1/17.png" alt="17" border="0"></a>
-<a href="https://ibb.co/pKCKy3Z"><img src="https://i.ibb.co/t2y2Xbc/18.png" alt="18" border="0"></a>
-<a href="https://ibb.co/C11QV0P"><img src="https://i.ibb.co/Fxxz87H/19.png" alt="19" border="0"></a>
-<a href="https://ibb.co/RbQ4Kks"><img src="https://i.ibb.co/T2PqGx7/20.png" alt="20" border="0"></a>
-<a href="https://ibb.co/QbsctLk"><img src="https://i.ibb.co/VjXN5Rx/21.png" alt="21" border="0"></a>
-<a href="https://ibb.co/c1ntb3v"><img src="https://i.ibb.co/W6JFD53/22.png" alt="22" border="0"></a>`
+const thumnailLinks = `<a href="https://imgbb.com/"><img src="https://i.ibb.co/51Cx4RT/1.png" alt="1" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/8sGh4pn/2.png" alt="2" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/zXT0zmH/3.png" alt="3" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/rHkzWG8/4.png" alt="4" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/RzYFkjC/5.png" alt="5" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/Jk471kf/6.png" alt="6" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/mqyg2Z2/7.png" alt="7" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/LCVT0jM/8.png" alt="8" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/yQNxZVM/9.png" alt="9" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/7brCsm2/10.png" alt="10" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/MZ3G279/11.png" alt="11" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/fXQJ3js/12.png" alt="12" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/YjCdnsq/13.png" alt="13" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/ft2gnpk/14.png" alt="14" border="0"></a>
+<a href="https://ibb.co/P6kbnXC"><img src="https://i.ibb.co/TgynSjh/15.png" alt="15" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/Q9yksNn/16.png" alt="16" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/T8PPRTV/17.png" alt="17" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/BjMqNw0/18.png" alt="18" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/BZ9X91T/19.png" alt="19" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/WHwckNg/20.png" alt="20" border="0"></a>
+<a href="https://ibb.co/5c6Y3dT"><img src="https://i.ibb.co/RH9hK12/21.png" alt="21" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/1bpN3M0/22.png" alt="22" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/bL4Rbmw/23.png" alt="23" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/VVFHSpJ/24.png" alt="24" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/CHzLCGS/25.png" alt="25" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/k4vbKKG/26.png" alt="26" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/1ZJ9DVp/27.png" alt="27" border="0"></a>
+<a href="https://imgbb.com/"><img src="https://i.ibb.co/CwL3y0m/28.png" alt="28" border="0"></a>`
+const imageLinks = `<a href="https://ibb.co/0CXxRxw"><img src="https://i.ibb.co/wSLfFfv/1.png" alt="1" border="0"></a>
+<a href="https://ibb.co/yqg4m7y"><img src="https://i.ibb.co/dDkmsx2/2.png" alt="2" border="0"></a>
+<a href="https://ibb.co/D98Y8wV"><img src="https://i.ibb.co/9bVhVy8/3.png" alt="3" border="0"></a>
+<a href="https://ibb.co/Nn6YvkX"><img src="https://i.ibb.co/jZv4Nxd/4.png" alt="4" border="0"></a>
+<a href="https://ibb.co/1LnDXWb"><img src="https://i.ibb.co/nb0H1JR/5.png" alt="5" border="0"></a>
+<a href="https://ibb.co/VYx3VRn"><img src="https://i.ibb.co/HTCX76w/6.png" alt="6" border="0"></a>
+<a href="https://ibb.co/pvgB50K"><img src="https://i.ibb.co/svhdzm1/7.png" alt="7" border="0"></a>
+<a href="https://ibb.co/D5VTLLn"><img src="https://i.ibb.co/XS2MVVT/8.png" alt="8" border="0"></a>
+<a href="https://ibb.co/vzh97f0"><img src="https://i.ibb.co/YjXgzJH/9.png" alt="9" border="0"></a>
+<a href="https://ibb.co/rdxWrYb"><img src="https://i.ibb.co/9ZbM56w/10.png" alt="10" border="0"></a>
+<a href="https://ibb.co/kS75wkN"><img src="https://i.ibb.co/nLG6YZx/11.png" alt="11" border="0"></a>
+<a href="https://ibb.co/f8rS54J"><img src="https://i.ibb.co/9wyvLsP/12.png" alt="12" border="0"></a>
+<a href="https://ibb.co/dLXCjcx"><img src="https://i.ibb.co/b2YwQ3D/13.png" alt="13" border="0"></a>
+<a href="https://ibb.co/vXyLxq4"><img src="https://i.ibb.co/z69PhxV/14.png" alt="14" border="0"></a>
+<a href="https://ibb.co/DCDD9PY"><img src="https://i.ibb.co/1qffrYM/15.png" alt="15" border="0"></a>
+<a href="https://ibb.co/FwKxynS"><img src="https://i.ibb.co/sw2mNt0/16.png" alt="16" border="0"></a>
+<a href="https://ibb.co/km71byy"><img src="https://i.ibb.co/nPGCd00/17.png" alt="17" border="0"></a>
+<a href="https://ibb.co/jZnRDP8"><img src="https://i.ibb.co/6JjBF7y/18.png" alt="18" border="0"></a>
+<a href="https://ibb.co/YRwvcLr"><img src="https://i.ibb.co/FxCG5DP/19.png" alt="19" border="0"></a>
+<a href="https://ibb.co/hRT14b9"><img src="https://i.ibb.co/zhtHBpx/20.png" alt="20" border="0"></a>
+<a href="https://ibb.co/M685Hm3"><img src="https://i.ibb.co/FVsq1Fj/21.png" alt="21" border="0"></a>
+<a href="https://ibb.co/K7CxRj1"><img src="https://i.ibb.co/tcypkMd/22.png" alt="22" border="0"></a>
+<a href="https://ibb.co/tzK8cGN"><img src="https://i.ibb.co/1MQ8Kgp/23.png" alt="23" border="0"></a>
+<a href="https://ibb.co/BqwN22q"><img src="https://i.ibb.co/t83Xpp8/24.png" alt="24" border="0"></a>
+<a href="https://ibb.co/jWCbnMg"><img src="https://i.ibb.co/KLtbTqF/25.png" alt="25" border="0"></a>
+<a href="https://ibb.co/f18XNHK"><img src="https://i.ibb.co/kH3GDXr/26.png" alt="26" border="0"></a>
+<a href="https://ibb.co/r0cGgDc"><img src="https://i.ibb.co/C8M5qRM/27.png" alt="27" border="0"></a>
+<a href="https://ibb.co/MfzR0Sv"><img src="https://i.ibb.co/7Q3GmCf/28.png" alt="28" border="0"></a>`
 
 const arrThumnailsEmbed = thumnailLinks.split(`\n`)
 const arrImageEmbed = imageLinks.split(`\n`)
@@ -89,10 +101,10 @@ const result = syncImageLinksToObject()
 setTimeout(() => {
   const text = result
     .map(JSON.stringify)
-    .reduce((prev, next) => `[${prev},\n${next}]`)
+    .reduce((prev, next) => `${prev},\n${next}`)
   document.write(text)
   // const data = new Blob([text], { type: 'text/plain' })
   // const link = document.getElementById('downloadlink')
   // link.href = window.URL.createObjectURL(data)
   // link.style.display = 'block'
-}, 500)
+}, 5000)
