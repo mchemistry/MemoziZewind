d9c15fb3acff4b1b6c8b4386f4ce76b9ecfc29c1
Tân Dv <mchemistry95@gmail.com>
2020-12-16 22:02:09 +0700

    Update slider track

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,14 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    // we only want single quotes
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: [2, 'never'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error'],
+  },
 }
--- components/Test/Test.vue
+++ components/Test/Test.vue
@@ -1,33 +1,144 @@
 <template>
-  <div class="card"></div>
+  <div>
+    <input v-model="value" type="range" min="0" max="1000" />
+    <span class="mt-4">Value is {{ value }}</span>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      value: 0,
+    }
+  },
+}
 </script>
 
-<style scoped>
-@keyframes flipped {
-  0% {
-    transform: rotateY(0deg);
+<style lang="scss" scoped>
+$input-slider-height: 18px;
+$slider-width-number: 240;
+$slider-width: #{$slider-width-number}px;
+$slider-height: 2px;
+$background-slider: #c7c7c7;
+$background-filled-slider: #e33d44;
+$thumb-width: 10px;
+$thumb-height: 10px;
+$thumb-radius: 5px;
+$thumb-background: #fff;
+$thumb-border: 1px solid #777;
+$shadow-size: -8px;
+$fit-thumb-in-slider: -4px;
+$loading-progress-width: 50%;
+$loading-progress-height: 2px;
+$loading-progress-background-color: #fa9a9f;
+$loading-progress-fit: 0;
+
+@function makelongshadow($color, $size) {
+  $val: 5px 0 0 $size $color;
+
+  @for $i from 6 through $slider-width-number {
+    $val: #{$val}, #{$i}px 0 0 $size #{$color};
   }
-  100% {
-    transform: rotateY(180deg);
+
+  @return $val;
+}
+
+div {
+  height: 100px;
+  width: 240px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+input[type='range'] {
+  align-items: center;
+  appearance: none;
+  background: none;
+  cursor: pointer;
+  display: flex;
+  overflow: hidden;
+  width: $slider-width;
+  height: $input-slider-height;
+  &:focus {
+    box-shadow: none;
+    outline: none;
   }
-}
-.card {
-  position: absolute;
-  width: 300px;
-  height: 300px;
-  border-radius: 50%;
-  z-index: 1;
-  background: #161122;
-  box-shadow: 6px 6px 6px #0f1a24, -4px -4px 4px #1f2327;
-  padding: 2px;
-  background-image: url('https://picsum.photos/200/300');
-}
-.card:hover {
-  animation: flipped 1s forwards;
-  transform-style: preserve-3d;
+
+  &::-webkit-slider-thumb {
+    width: $thumb-width;
+    height: $thumb-height;
+    appearance: none;
+    background: $thumb-background !important;
+    left: 4px;
+    border-radius: $thumb-radius;
+    box-shadow: makelongshadow($background-slider, $shadow-size);
+    margin-top: $fit-thumb-in-slider;
+  }
+
+  &::-webkit-slider-runnable-track {
+    background: $background-filled-slider;
+    content: '';
+    height: $slider-height;
+    pointer-events: none;
+    margin-top: $fit-thumb-in-slider;
+  }
+
+  &::-moz-range-track {
+    width: $slider-width;
+    height: $slider-height;
+  }
+
+  &::-moz-range-thumb {
+    width: $thumb-width;
+    height: $thumb-height;
+    background: $thumb-background;
+    border-radius: $thumb-radius;
+    border: $thumb-border;
+    position: relative;
+  }
+
+  &::-moz-range-progress {
+    height: $slider-height;
+    background: $background-filled-slider;
+    border: 0;
+    margin-top: 0;
+  }
+
+  &::-ms-track {
+    background: red;
+    border: 0;
+    border-color: red;
+    border-radius: 0;
+    border-width: 0;
+    color: red;
+    height: $slider-height;
+    margin-top: 12px;
+    width: 150%;
+  }
+
+  &::-ms-thumb {
+    width: $thumb-width;
+    height: $thumb-height;
+    background: $thumb-background;
+    border-radius: $thumb-radius;
+    border: $thumb-border;
+    display: none;
+  }
+
+  &::-ms-fill-lower {
+    background: $background-filled-slider;
+    border-radius: 0;
+  }
+
+  &::-ms-fill-upper {
+    background: $background-slider;
+    border-radius: 0;
+  }
+
+  &::-ms-tooltip {
+    display: none;
+  }
 }
 </style>
